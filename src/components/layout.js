137e7ea359186c949ebe554073ade1be1a87b528
Zain Fathoni <zain.fathoni@gmail.com>
2019-04-19 00:15:05 +0800

    Reformat JS files using the new ESLint rules

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header'
+import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -31,14 +31,14 @@ const Layout = ({ children }) => (
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href='https://www.gatsbyjs.org'>Gatsby</a>
           </footer>
         </div>
       </>
@@ -47,7 +47,7 @@ const Layout = ({ children }) => (
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired
 }
 
 export default Layout
