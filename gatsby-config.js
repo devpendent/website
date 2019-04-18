d5d4f36fc15cb79fc7255ba53019499d3c5ae37e
Zain Fathoni <zain.fathoni@gmail.com>
2019-04-19 00:46:57 +0800

    Whitelabel siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`
+    title: `Devpendent`,
+    description: `👨🏻‍💻 Open Sourced Platform for Indonesian Election Real Count`,
+    author: `@zainfathoni`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
