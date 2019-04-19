3a61e49ca68e7f39189cf96ea3aa697795f2b231
Zain Fathoni <zain.fathoni@gmail.com>
2019-04-19 12:53:28 +0800

    Format gatsby-*.js files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,18 +5,18 @@
  */
 
 // You can delete this file if you're not using it
-const path = require("path")
+const path = require('path')
 
 exports.onCreateWebpackConfig = ({ actions, loaders, plugins, stage }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: ['src', 'node_modules'],
       alias: {
-        "_variables.sass": path.resolve(
+        '_variables.sass': path.resolve(
           __dirname,
-            "./src/styles/_variables.sass"
-        ),
-      },
-    },
+          './src/styles/_variables.sass'
+        )
+      }
+    }
   })
 }
