e0031d71f36c563c5951de002d1f52f60655ffe1
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-04-21 03:22:49 +0700

    improvement : revise test functionality

--- cypress/e2e/test.js
+++ cypress/e2e/test.js
@@ -1,27 +1,29 @@
-import meta from "../../gatsby-config.js";
+import meta from '../../gatsby-config.js';
 
-describe("create-react-app default app", () => {
-  it("shows the default home page of devpendant apps", () => {
-    cy.visit("/")
+describe('Simple Devpendant e2e Test', () => {
+  it('shows the default home page of devpendant apps', () => {
+    cy.visit('/')
       .url()
-      .should("eq", `${Cypress.config().baseUrl}`);
+      .should('eq', `${Cypress.config().baseUrl}`);
   });
 
-  it("shows the Banner of devpendant apps", () => {
-    cy.contains(meta.siteMetadata.title).should("be.visible");
+  it('shows the Banner of devpendant apps', () => {
+    cy.contains(meta.siteMetadata.title).should('be.visible');
     cy.contains(meta.siteMetadata.title);
-    cy.get("h1").should("contain", meta.siteMetadata.title);
+    cy.get('h1').should('contain', meta.siteMetadata.title);
   });
 
-  it("shows the title of devpendant apps", () => {
-    cy.title().should("contain", meta.siteMetadata.title);
+  it('shows the title of devpendant apps', () => {
+    cy.title().should('contain', meta.siteMetadata.title);
   });
 
-  it("navigating to page-2", () => {
-    cy.visit("page-2");
+  it('navigating to page-2', () => {
+    cy.get('a').should('contain', 'page 2');
+    cy.get('main > a').click();
   });
 
-  it("navigating back to home", () => {
-    cy.visit("/");
+  it('navigating back to home', () => {
+    cy.get('a').should('contain', 'homepage');
+    cy.get('main > a').click();
   });
 });
