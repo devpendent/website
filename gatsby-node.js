835be84d10f3686b763494f15c962ee30fdf66c9
ri7nz <ri7nz.labs@gmail.com>
2019-04-22 22:22:24 +0700

    Checkout removed configuration

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,13 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path')
+
+exports.onCreateWebpackConfig = ({ actions, loaders, plugins, stage }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: ['src', 'node_modules'],
+      alias: {}
+    }
+  })
+}
