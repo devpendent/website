dbb46eb169b15511461c53967f554e1be61ef05e
Zain Fathoni <zain.fathoni@gmail.com>
2019-05-03 23:35:12 +0800

    Add Submit page

---
+++ src/components/submit-form.js
@@ -0,0 +1,10 @@
+import { Form } from 'antd'
+import React from 'react'
+
+const SubmitForm = () => (
+  <Form colon={false} layout='vertical'>
+    <Form.Item label='Upload' />
+  </Form>
+)
+
+export default SubmitForm
