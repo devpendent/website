e352f943fb4e329198a946fde5f50d40b2db251c
Zain Fathoni <zain.fathoni@gmail.com>
2019-05-04 00:42:00 +0800

    Refactor Layout component & remove css-prop test

--- src/components/__tests__/layout.test.js
+++ src/components/__tests__/layout.test.js
@@ -1,7 +1,10 @@
 import { useStaticQuery } from 'gatsby'
+import serializer from 'jest-emotion'
 import React from 'react'
 import { render } from 'react-testing-library'
 import Layout from '../layout'
+
+expect.addSnapshotSerializer(serializer)
 
 describe('Layout', () => {
   beforeEach(() => {
@@ -22,6 +25,6 @@ describe('Layout', () => {
         <h1>Devpendent Website</h1>
       </Layout>
     )
-    expect(container).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,11 +5,20 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
+import styled from '@emotion/styled'
 import Header from 'components/header'
 import { graphql, useStaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 import './layout.css'
+
+const Div = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+`
+
+const Main = styled.main``
 
 const Layout = ({ children }) => {
   const {
@@ -29,21 +38,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={title} description={description} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0
-        }}
-      >
-        <main>{children}</main>
+      <Div>
+        <Main>{children}</Main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href='https://www.gatsbyjs.org'>Gatsby</a>
         </footer>
-      </div>
+      </Div>
     </>
   )
 }
