539c715a798af2ebe14f3bee6c909779da113a98
Zain Fathoni <zain.fathoni@gmail.com>
2019-05-04 01:59:55 +0800

    Add Submit button

--- src/components/__tests__/submit-form.test.js
+++ src/components/__tests__/submit-form.test.js
@@ -1,6 +1,6 @@
 import SubmitForm from 'components/submit-form'
 import React from 'react'
-import { fireEvent, render } from 'react-testing-library'
+import { fireEvent, render, waitForElement } from 'react-testing-library'
 
 describe('SubmitForm', () => {
   it('renders Calon A field as a number', () => {
@@ -21,5 +21,28 @@ describe('SubmitForm', () => {
     expect(calonA).toHaveProperty('value', '')
     fireEvent.change(calonA, { target: { value: '123' } })
     expect(calonA).toHaveProperty('value', '123')
+  })
+
+  it('validates field requirements correctly', async () => {
+    const { getByText } = render(<SubmitForm />)
+    const warn = jest.spyOn(console, 'warn').mockImplementation(() => {})
+
+    const submit = getByText('Kirim')
+    fireEvent.submit(submit)
+
+    const errorCalonA = await waitForElement(() =>
+      getByText('Masukkan total perolehan suara Calon A'))
+    expect(errorCalonA).toBeVisible()
+    expect(getByText('Masukkan total perolehan suara Calon B')).toBeVisible()
+
+    expect(warn).toHaveBeenCalledTimes(2)
+    expect(warn).toHaveBeenNthCalledWith(1, 'async-validator:', [
+      'candidateA is required'
+    ])
+    expect(warn).toHaveBeenNthCalledWith(2, 'async-validator:', [
+      'candidateB is required'
+    ])
+
+    warn.mockRestore()
   })
 })
--- src/components/submit-form.js
+++ src/components/submit-form.js
@@ -1,30 +1,52 @@
-import { Form, Input } from 'antd'
+import { Button, Form, Input } from 'antd'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const _SubmitForm = ({ form: { getFieldDecorator } }) => (
-  <Form colon={false} layout='vertical'>
-    <Form.Item label='Upload' />
-    <Form.Item label='Calon A'>
-      {getFieldDecorator('candidateA', {
-        rules: [
-          { message: 'Masukkan total perolehan suara Calon A', required: true }
-        ]
-      })(<Input placeholder='Total suara Calon A' type='number' />)}
-    </Form.Item>
-    <Form.Item label='Calon B'>
-      {getFieldDecorator('candidateB', {
-        rules: [
-          { message: 'Masukkan total perolehan suara Calon B', required: true }
-        ]
-      })(<Input placeholder='Total suara Calon B' type='number' />)}
-    </Form.Item>
-  </Form>
-)
+const _SubmitForm = ({ form: { getFieldDecorator, validateFields } }) => {
+  const handleSubmit = e => {
+    e.preventDefault()
+    validateFields((err, values) => {
+      if (!err) {
+      }
+    })
+  }
+
+  return (
+    <Form colon={false} layout='vertical' onSubmit={handleSubmit}>
+      <Form.Item label='Upload' />
+      <Form.Item label='Calon A'>
+        {getFieldDecorator('candidateA', {
+          rules: [
+            {
+              message: 'Masukkan total perolehan suara Calon A',
+              required: true
+            }
+          ]
+        })(<Input placeholder='Total suara Calon A' type='number' />)}
+      </Form.Item>
+      <Form.Item label='Calon B'>
+        {getFieldDecorator('candidateB', {
+          rules: [
+            {
+              message: 'Masukkan total perolehan suara Calon B',
+              required: true
+            }
+          ]
+        })(<Input placeholder='Total suara Calon B' type='number' />)}
+      </Form.Item>
+      <Form.Item>
+        <Button htmlType='submit' type='primary'>
+          Kirim
+        </Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
 _SubmitForm.propTypes = {
   form: PropTypes.shape({
-    getFieldDecorator: PropTypes.func.isRequired
+    getFieldDecorator: PropTypes.func.isRequired,
+    validateFields: PropTypes.func.isRequired
   }).isRequired
 }
 
