300718285db666e8aa7071e569c1101998b2cc26
Muhammad Irwan Andriawan <andriawan2014@gmail.com>
2019-05-13 17:40:00 +0700

    improve blog template layout & style

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,22 +1,25 @@
+import Layout from 'components/layout'
+import SEO from 'components/seo'
 import { graphql } from 'gatsby'
 import React from 'react'
 
-export default function Template ({
-  data // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds our post data
+const Template = ({ data }) => {
+  const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className='blog-post-container'>
-      <div className='blog-post'>
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className='blog-post-content'
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <Layout>
+      <SEO title='Posts' />
+      <h1>{frontmatter.title}</h1>
+      <div className='blog-post-container'>
+        <div className='blog-post'>
+          <h2>{frontmatter.date}</h2>
+          <div
+            className='blog-post-content'
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
@@ -32,3 +35,4 @@ export const pageQuery = graphql`
     }
   }
 `
+export default Template
